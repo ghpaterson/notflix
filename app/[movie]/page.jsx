3b54245f49e4fe86858029aa479356d7c3a9e493
Graeme Paterson <ghpaterson@gmail.com>
2023-03-20 21:21:39 +0000

    movie page general description added from api

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,10 +1,18 @@
 import Image from "next/image"
 
+export async function generateStaticParams(){
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+    const res = await data.json()
+    return res.results.map((movie) => ({
+        movie: toString(movie.id),
+    }))
+}
+
 export default async function MovieDetail({params}){
     const {movie} = params
     const imagePath = 'https://image.tmdb.org/t/p/original'
 
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate: 0 }})
 
     const res = await data.json()
     return(
@@ -21,6 +29,7 @@ export default async function MovieDetail({params}){
             height={800} 
             priority
             />
+            <p>{res.overview}</p>
             </div>
         </div>
     )
