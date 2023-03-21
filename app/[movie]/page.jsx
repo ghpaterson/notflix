d2ab927957204ef66b42fd0800c51a6533eff7f8
Graeme Paterson <ghpaterson@gmail.com>
2023-03-21 16:55:32 +0000

    layout chnages to individual movie title page

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -16,23 +16,28 @@ export default async function MovieDetail({params}){
 
     const res = await data.json()
     return(
-        <div>
-            <div>
-                <h2>{res.title}</h2>
-                <h2>{res.release_date}</h2>
-                <h2>Length: {res.runtime} mins</h2>
-                <h2>{res.status}</h2>
+        <div className="flex justify-start gap-20 my-20">
+          <div>
+              <Image 
+              className="rounded-lg" 
+              src={imagePath + res.poster_path}
+              width={300} 
+              height={300} 
+              priority
+               />
+          </div>
+          <div className=" font-noir text-xl">
+            <h2 className="text-3xl">{res.title}</h2>
+            <h2>{res.release_date}</h2>
+            <h2>Length: {res.runtime} mins</h2>
+            <h2 className=" bg-green-700 rounded-xl text-center text-gray-100 w-24">{res.status}</h2>
+              <div className=" h-48 w-96 mt-10 flex flex-col">
                 <p>{res.overview}</p>
-            </div>
-            <div>
-                <Image 
-                className="my-12" 
-                src={imagePath + res.poster_path}
-                width={400} 
-                height={400} 
-                priority
-                />
-            </div>
-        </div>
+              </div>
+              <div className="flex items-center justify-center text-xl text-gray-100 w-36 h-8 rounded-xl bg-red-500 my-10">
+                <a href="#">Watch Trailer</a>
+              </div>
+          </div>
+      </div>
     )
 }
